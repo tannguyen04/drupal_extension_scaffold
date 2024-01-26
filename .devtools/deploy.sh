2ef668cf404564246f5246bc49bc948cbf369c25
tan.nguyen <raucaixanh04@gmail.com>
2024-01-26 14:12:18 +0700

    test

--- .devtools/deploy.sh
+++ .devtools/deploy.sh
@@ -55,16 +55,15 @@ echo "-------------------------------"
 
 [ "${DEPLOY_PROCEED}" != "1" ] && echo "> Skip deployment because $DEPLOY_PROCEED is not set to 1" && exit 0
 
-echo "> Configure git and SSH to connect to remote servers for deployment."
-ls -al "${HOME}/.ssh/"
-mkdir -p "${HOME}/.ssh/"
-echo -e "Host *\n\tStrictHostKeyChecking no\n" >"${HOME}/.ssh/config"
-DEPLOY_SSH_FILE="${DEPLOY_SSH_FINGERPRINT//:/}"
-DEPLOY_SSH_FILE="${HOME}/.ssh/id_rsa_${DEPLOY_SSH_FILE//\"/}"
-[ ! -f "${DEPLOY_SSH_FILE:-}" ] && echo "ERROR: Unable to find Deploy SSH key file ${DEPLOY_SSH_FILE}." && exit 1
-if [ -z "${SSH_AGENT_PID:-}" ]; then eval "$(ssh-agent)"; fi
-ssh-add -D >/dev/null
-ssh-add "${DEPLOY_SSH_FILE}"
+# echo "> Configure git and SSH to connect to remote servers for deployment."
+# mkdir -p "${HOME}/.ssh/"
+# echo -e "Host *\n\tStrictHostKeyChecking no\n" >"${HOME}/.ssh/config"
+# DEPLOY_SSH_FILE="${DEPLOY_SSH_FINGERPRINT//:/}"
+# DEPLOY_SSH_FILE="${HOME}/.ssh/id_rsa_${DEPLOY_SSH_FILE//\"/}"
+# [ ! -f "${DEPLOY_SSH_FILE:-}" ] && echo "ERROR: Unable to find Deploy SSH key file ${DEPLOY_SSH_FILE}." && exit 1
+# if [ -z "${SSH_AGENT_PID:-}" ]; then eval "$(ssh-agent)"; fi
+# ssh-add -D >/dev/null
+# ssh-add "${DEPLOY_SSH_FILE}"
 
 echo "> Configure git user name and email, but only if not already set."
 [ "$(git config --global user.name)" == "" ] && echo "> Configure global git user name ${DEPLOY_USER_NAME}." && git config --global user.name "${DEPLOY_USER_NAME}"
